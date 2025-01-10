37ffe6c31210defcdd9603571296b51844d173db
Shishir <shishirsrivastava506@gmail.com>
2025-01-11 02:22:36 +0530

    removed dates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,10 +92,8 @@ export default function Page() {
                   key={project.title}
                   title={project.title}
                   description={project.description}
-                  dates={project.dates}
                   tags={project.technologies}
                   image={project.image}
-                  video={project.video}
                   links={project.links}
                 />
               </BlurFade>
